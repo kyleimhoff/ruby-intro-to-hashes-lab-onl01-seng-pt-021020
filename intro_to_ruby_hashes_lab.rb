5695058fc99e5f47cd5fca6aeff8de7e663dc1d3
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-22 18:57:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,18 +7,20 @@ actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {railroads: {}}
+	monopoly = {railroads: {
+	  pieces: 4,
+	  :rent_in_dollars => {}
+	  :name => {}
+	}}
 
 end
 
 def monopoly_with_second_tier
 	monopoly = {railroads: {
 	  pieces: 4,
+	  :rent_in_dollars => {}
 	  :name => {}
-	  
-	}
-	  
-	}
+	}}
 
 end
 
