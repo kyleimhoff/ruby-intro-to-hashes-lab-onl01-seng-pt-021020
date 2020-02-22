19aac5b1b246d563c47364b1ec5b1ec91790a325
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-22 19:00:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly
 	monopoly = {railroads: {
 	  pieces: 4,
 	  :rent_in_dollars => {}
-	  :name => {}
+	  :names => {}
 	}}
 
 end
@@ -19,7 +19,7 @@ def monopoly_with_second_tier
 	monopoly = {railroads: {
 	  pieces: 4,
 	  :rent_in_dollars => {},
-	  :name => {}
+	  :names => {}
 	}}
 
 end
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	monopoly = {railroads: {
 	  pieces: 4,
 	  :rent_in_dollars => {},
-	  :name => {}
+	  :names => {}
 	  
 	}
 	  
